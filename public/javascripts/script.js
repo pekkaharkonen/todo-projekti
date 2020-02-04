c752322cb1cd185eeac98ca79548612b43569568
Pekka Härkönen <pekka.o.harkonen@utu.fi>
2020-02-04 13:05:54 +0200

    alustava ulkoasu tehty web-sivulle

---
+++ public/javascripts/script.js
@@ -0,0 +1,11 @@
+const task = document.getElementById('task')
+const button = document.getElementById('button')
+
+button.addEventListener("click", sendData)
+
+function sendData(e){
+
+  
+  // Prevent default behavior
+  e.preventDefault();
+}
