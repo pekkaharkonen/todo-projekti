54139907df7d3de64bd1890d3418070226cc6764
Pekka Härkönen <pekka.o.harkonen@utu.fi>
2020-02-04 13:20:44 +0200

    js lomakedatan lahettamiselle kirjoitettu alustavasti

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,27 @@
-const task = document.getElementById('task')
-const button = document.getElementById('button')
+const taskData = document.getElementById("task");
+const button = document.getElementById("button");
 
-button.addEventListener("click", sendData)
+button.addEventListener("click", sendData);
 
-function sendData(e){
+function sendData(e) {
+  //Lähetetään form data Fetch POSTilla:
+  fetch("/api/tasks", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      task: taskData.value
+    })
+  })
+    .then(res => {
+      if (res.status === 201){
+        console.log("Post request successful!")
+      }
+      return res.json();
+    })
+    .then(data => console.log(data));
 
-  
-  // Prevent default behavior
+  // Varmuuden vuoksi estetään tavallisesti tapahtuva:
   e.preventDefault();
 }
