91f1760e36cd55aa428538a4c2f0dfdf4d1699ac
Pekka Härkönen <pekka.o.harkonen@utu.fi>
2020-02-04 14:02:38 +0200

    pienia parannuksia frontendin tiedostoihin

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,9 +1,14 @@
 const taskData = document.getElementById("task");
 const button = document.getElementById("button");
 
-button.addEventListener("click", sendData);
+button.addEventListener("click", postTask);
+window.addEventListener("DOMContentLoaded", getTasks);
 
-function sendData(e) {
+function postTask(e) {
+  if (taskData.value === ""){
+    alert("Please add a task")
+    return;
+  }
   //Lähetetään form data Fetch POSTilla:
   fetch("/api/tasks", {
     method: "POST",
@@ -15,13 +20,29 @@ function sendData(e) {
     })
   })
     .then(res => {
-      if (res.status === 201){
-        console.log("Post request successful!")
+      if (res.status === 201) {
+        console.log("Post request successful!");
       }
       return res.json();
     })
-    .then(data => console.log(data));
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
 
   // Varmuuden vuoksi estetään tavallisesti tapahtuva:
   e.preventDefault();
 }
+
+async function getTasks() {
+  try {
+    const response = await fetch("/api/tasks");
+    const fetchedTasks = await response.json();
+    console.log(fetchedTasks);
+    addTasksToSite(fetchedTasks);
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function addTasksToSite(tasks) {}
+
+
