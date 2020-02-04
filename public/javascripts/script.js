e14b0085aa59dc0de5e99d13a4b44b1306d3c0bf
Pekka Härkönen <pekka.o.harkonen@utu.fi>
2020-02-04 16:31:49 +0200

    front endin js toiminnallisuutta lisätty

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,14 @@
 const taskData = document.getElementById("task");
 const button = document.getElementById("button");
-const taskList = document.getElementById('task-list')
+const taskList = document.getElementById("task-list");
 
 button.addEventListener("click", postTask);
 window.addEventListener("DOMContentLoaded", getTasks);
+taskList.addEventListener("click", updateOrDeleteTask);
 
 function postTask(e) {
-  if (taskData.value === ""){
-    alert("Please add a task")
+  if (taskData.value === "") {
+    alert("Please add a task");
     return;
   }
   //Lähetetään form data Fetch POSTilla:
@@ -29,6 +30,7 @@ function postTask(e) {
     .then(data => console.log(data))
     .catch(error => console.log(error));
 
+  location.reload();
   // Varmuuden vuoksi estetään tavallisesti tapahtuva:
   e.preventDefault();
 }
@@ -40,16 +42,47 @@ async function getTasks() {
     console.log(fetchedTasks);
     addTasksToSite(fetchedTasks);
   } catch (error) {
-    console.log(error)
+    console.log(error);
+  }
+}
+
+function updateOrDeleteTask(e) {
+  // Haetaan taskin arvot event-parametrilta
+  let taskName = e.target.parentElement.parentElement.firstChild.innerText;
+  let taskId = e.target.parentElement.parentElement.firstChild.id;
+
+  // Kutsutaan funktiota updateTask tai deleteTask riippuen siitä, kumpaa nappia ollaan painettu
+
+  if (e.target.innerHTML === "Update") {
+    updateTask(taskName, taskId);
+  }
+  if (e.target.innerHTML === "Delete") {
+    deleteTask(taskId);
   }
 }
 
 function addTasksToSite(tasks) {
-  for (let t of tasks){
-    let li = document.createElement('li')
-    li.innerHTML = t.task
-    taskList.appendChild(li)
+  for (let t of tasks) {
+    console.log(t);
+    let li = document.createElement("li");
+    li.innerHTML = `<p id="${t.id}">${t.task}</p> <span><button id="update-task">Update</button><button id="delete-task">Delete</button></span>`;
+    taskList.appendChild(li);
   }
 }
 
+function deleteTask(id) {
+  fetch(`/api/tasks/${id}`, {
+    method: "DELETE",
+  })
+    .then(res => {
+      if (res.status === 200) {
+        console.log("Delete request successful!");
+      }
+      return res.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
 
+    location.reload();
+}
+function updateTask(task, id) {}
