019111a790a241eb07f8ad68c36465c3bf091eb8
Titta Kujala <titta.kujala@gmail.com>
2020-02-05 13:04:12 +0200

    weather-routes tehty

--- routes/tasks.js
+++ routes/tasks.js
@@ -52,7 +52,7 @@ router.route('/:id')
   .delete(function (req, res) {
     for (let task of tasks) {
       if (task.id === req.params.id) {
-        tasks.splice(task, 1)
+        tasks.splice(tasks.indexOf(task), 1)
         write();
         res.json({ Viesti: "Taski poistettu!" })
         return
@@ -78,8 +78,5 @@ router.route('/:id')
     res.status(404)
     res.json({ Viesti: "Virhe! Taskia ei löytynyt" })
   });
-
-
-
 
 module.exports = router;
