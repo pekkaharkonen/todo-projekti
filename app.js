fbf84a4d62486a1f23093c911272fabb9d234ade
Pekka Härkönen <pekka.o.harkonen@utu.fi>
2020-02-06 13:03:15 +0200

    testit lisätty REST APIlle, pug-tiedostot poistettu

--- app.js
+++ app.js
@@ -11,11 +11,6 @@ var tasksRouter = require('./routes/tasks'); //
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-//tasks = []; //
 
 app.use(logger('dev'));
 app.use(express.json());
